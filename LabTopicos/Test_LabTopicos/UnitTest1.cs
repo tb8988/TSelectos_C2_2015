6bb44b3ce150a425e6b16e04edbd49681ba12b24
tb8988 <tb8988@gmail.com>
2015-07-11 15:27:24 -0600

    Testing_Int_Simple

--- LabTopicos/Test_LabTopicos/UnitTest1.cs
+++ LabTopicos/Test_LabTopicos/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LabTopicos_app;
 
 namespace Test_LabTopicos
 {
@@ -10,6 +11,18 @@ namespace Test_LabTopicos
         public void Test_Integral_Simple()
         {
             //Valores de prueba
+            double var1 = 1;
+            double var2 = 5;
+            double result_esperado = 0;
+            Integral integral = new Integral();
+
+
+            //Accion
+            result_esperado = integral.Simple(var1, var2);
+
+            //Resultado
+            double result_real = 4;
+            Assert.AreEqual(result_esperado,result_real, 0.0001,"Resultado erroneo");
 
         }
     }
