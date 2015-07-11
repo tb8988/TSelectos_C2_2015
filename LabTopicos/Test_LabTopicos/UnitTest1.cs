352456951d7c5035d78a879dd3e7688a6822e0a6
tb8988 <tb8988@gmail.com>
2015-07-11 15:02:11 -0600

    Testing

--- LabTopicos/Test_LabTopicos/UnitTest1.cs
+++ LabTopicos/Test_LabTopicos/UnitTest1.cs
@@ -7,8 +7,10 @@ namespace Test_LabTopicos
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Test_Integral_Simple()
         {
+            //Valores de prueba
+
         }
     }
 }
