1b90bedcbde312b1dd3612f0f481c0893f35da88
djosue27 <djosue27@live.com>
2015-07-11 14:08:56 -0600

    Anadido las integrales

---
+++ LabTopicos/LabTopicos_app/Integral.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabTopicos_app
+{
+    class Integral
+    {
+
+        public void Simple() { 
+
+        }
+        public void Constante() { 
+
+        }
+        public void Potencia(){
+
+        }
+        public void Logaritmo() { 
+        
+        }
+        public void Exponencial() { 
+        
+        }
+        public void Seno() { 
+        
+        }
+        public void Coseno() { 
+        
+        }
+    }
+}
